2b272241fa5b83933df97ae7065014b41550f6cb
Yellapu MeghaShyam <22l61a6118@cec.ac.in>
2025-07-21 13:48:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://url-shortner-tau-beryl.vercel.app",
+  "https://url-shortner-shyam.vercel.app",
 ];
 
 app.use(
